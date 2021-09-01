63f336755e381ff40d711d9984c0e6473c7c9f68
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-09-01 14:17:24 -0300

    registration test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient, APITestCase
 
-# Create your tests here.
+from users.models import CustomUser
+
+
+class CreateUserTestCase(APITestCase):
+
+    def test_registration(self):
+        """Verifica se um usuário consegue se registrar no sistema"""
+        data = {'username': 'evoe',
+                'email':'evoe@localhost.com',
+                'password1': 'pythonDRF',
+                'password2': 'pythonDRF'
+        }
+        response = self.client.post(reverse('signup'), data)
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
