46512e23c44e3db63827e35c7e075e00c6e68555
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-09-01 13:56:29 -0300

    update users/views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import SignUpForm
+
+
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'users/signup.html'
