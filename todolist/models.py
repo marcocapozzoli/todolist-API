cc68a8224fff3c3bb6914c923dd26e6ca297507a
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-09-01 13:52:38 -0300

    create ToDoList model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class ToDoList(models.Model):
+    
+    CHECK = (
+        ('Y', 'Yes'),
+        ('N', 'No')
+    )
+    
+    task = models.CharField('Task', max_length=300)
+    date = models.DateField('Deadline')
+    check = models.CharField('Done', max_length=1, choices=CHECK, default='N', blank=False, null=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.task
