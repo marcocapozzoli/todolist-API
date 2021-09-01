d901fe99a318a00271d6e1ec6b9aeeb05097d37b
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-09-01 14:06:48 -0300

    update

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.authtoken import views
+from users.views import CustomLoginView, SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('todolist.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-token/', views.obtain_auth_token, name='token'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
