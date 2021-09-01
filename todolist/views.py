e959f485c547858eeb6bbf8a17d6f2ca664eceef
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-09-01 14:02:22 -0300

    Override update method in ToDoListViewSet

--- todolist/views.py
+++ todolist/views.py
@@ -23,3 +23,15 @@ class ToDoListViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         user = self.request.user
         serializer.save(user=user)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        if instance.user == request.user:
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            response = {'error': 'Você não tem permissão para editar essa tarefa.'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST) 
